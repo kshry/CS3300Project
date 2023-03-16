fe9482b721332d671e8e3964c3ca7f27c8701124
Keegan Shry <kshry@uccs.edu>
2023-03-16 00:54:49 +0000

    Added attribute validation to Character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,4 @@
 class Character < ApplicationRecord
+    validates :name, :character_class, :description, presence: true
+    validates :level, numericality: {greater_than_or_equal_to: 0}, presence: true
 end
