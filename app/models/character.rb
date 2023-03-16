50177aa1f4d505ccd83085d80fb5911690f64b6c
Keegan Shry <kshry@uccs.edu>
2023-03-16 16:50:51 +0000

    Added bootstrap

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,4 @@
 class Character < ApplicationRecord
-    validates :name, :character_class, :description, presence: true
+    validates :name, :character_class, presence: true
     validates :level, numericality: {greater_than_or_equal_to: 0}, presence: true
 end
